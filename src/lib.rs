a4a8a63c7eb1eee78d4639ab6d4c4f36a9a38679
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-11-10 17:34:56 +0100

    Make yup-oauth2 optional

--- src/lib.rs
+++ src/lib.rs
@@ -35,11 +35,13 @@ use std::{
   },
   time::{Duration, Instant},
 };
+#[cfg(feature = "yup-oauth2")]
+use tonic::MetadataValue;
 use tonic::{
-  metadata::MetadataValue,
   transport::{Channel, ClientTlsConfig},
   Request,
 };
+#[cfg(feature = "yup-oauth2")]
 use yup_oauth2::authenticator::Authenticator;
 
 pub mod proto {
@@ -267,11 +269,13 @@ pub trait Authorizer: Sync + Send + 'static {
   async fn authorize<T: Send + Sync>(&self, request: &mut Request<T>) -> Result<(), Self::Error>;
 }
 
+#[cfg(feature = "yup-oauth2")]
 pub struct YupAuthorizer {
   authenticator: Authenticator<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>>,
   project_id: String,
 }
 
+#[cfg(feature = "yup-oauth2")]
 impl YupAuthorizer {
   pub async fn new(
     credentials_path: impl AsRef<std::path::Path>,
@@ -291,6 +295,7 @@ impl YupAuthorizer {
   }
 }
 
+#[cfg(feature = "yup-oauth2")]
 #[async_trait]
 impl Authorizer for YupAuthorizer {
   type Error = Box<dyn std::error::Error + Send + Sync>;
